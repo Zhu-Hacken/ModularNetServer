cf3c9f04e5c2a032f31fd89203f37cc01eac9dba
Zhu-Hacken <983048285@qq.com>
2025-06-26 13:14:19 +0800

    新增WSConnManager全局单例，绑定fd与sessionId以使用sessionId进行数据帧发送

---
+++ src/conn/websocket/websocket_conn_manager.cpp
@@ -0,0 +1,48 @@
+#include "websocket_conn_manager.h"
+#include "log/logs.h"
+#include "net/net_server.h"
+#include "websocket_conn.h"
+const std::string& BASE_TEXT = "[WebsocketConnManager] ";
+
+WebsocketConnManager& WebsocketConnManager::getInstance() {
+    static WebsocketConnManager instance;
+    return instance;
+}
+
+bool WebsocketConnManager::bindSession( int fd, const SessionId& sessionId) {
+    std::lock_guard<std::mutex> lock(m_mutex);
+    m_session_to_fd[sessionId] = fd;
+    m_fd_to_session[fd] = sessionId;
+    return true;
+}
+
+bool WebsocketConnManager::sendToSession(const SessionId& sessionId, const std::string& msg) {
+    std::lock_guard<std::mutex> lock(m_mutex);
+    auto it = m_session_to_fd.find(sessionId);
+    if (it == m_session_to_fd.end()) return false;
+
+    int fd = it->second;
+
+    // 获取连接对象
+    auto conn = NetServer::getInstance().getConn(fd);
+    if (conn == nullptr) return false;
+    auto ws_conn = std::dynamic_pointer_cast<WebSocketConn>(conn);
+    if (!ws_conn || ws_conn->isClosed()) return false;
+
+    ws_conn->sendTextFrame(msg);
+    ws_conn->runEpollWriteCallback();
+    return true;
+}
+
+void WebsocketConnManager::removeFd(int fd) {
+    std::lock_guard<std::mutex> lock(m_mutex);
+    auto it = m_fd_to_session.find(fd);
+    if (it != m_fd_to_session.end()) {
+        m_session_to_fd.erase(it->second);
+        m_fd_to_session.erase(it);
+    }
+}
+
+bool WebsocketConnManager::isSessionAlive(const SessionId& sessionId) {
+    return SessionManager::getInstance().isSessionIdValid(sessionId);
+}
---
+++ src/conn/websocket/websocket_conn_manager.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "session/session_manager.h"
+#include <mutex>
+
+class WebsocketConnManager {
+
+public:
+    static WebsocketConnManager& getInstance();
+
+    // 绑定 fd 和 sessionId
+    bool bindSession( int fd, const SessionId& sessionId);
+    // 向 sessionId 的连接发送信息
+    bool sendToSession(const SessionId& sessionId, const std::string& msg);
+    // 移除 fd
+    void removeFd(int fd);
+    // 检查 session 是否有效
+    bool isSessionAlive(const SessionId& sessionId);
+
+private:
+    std::mutex  m_mutex;
+    std::unordered_map<SessionId, int>  m_session_to_fd;
+    std::unordered_map<int, SessionId> m_fd_to_session;
+
+};
